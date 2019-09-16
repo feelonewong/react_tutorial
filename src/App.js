dc840cff340e0e64a68e8b0215168c8ea814100b
feelonewong <feelonewong@gmail.com>
2019-09-16 08:07:02 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: '',
+      list: ['React','Vue','Angular']
+    }
+  }
+
+  render() {
+    const {inputValue,list} = this.state
+    return (
+      <div className="wrapper">
+        <input value={inputValue} type="text" onChange={this.handleChange.bind(this)}/>
+        <button onClick={this.handleSubmit.bind(this)}>Submit</button>
+        <ul className="my-list">
+          {list.map(
+            (item,index)=>{
+              return(
+                <li
+                  onClick={this.handleliClick.bind(this,index)}
+                  key={index}
+                >{item}</li>
+              )
+            }
+          )}
+        </ul>
+      </div>
+    )
+  }
+
+  handleChange(e) {
+    const inputValue = e.target.value
+    this.setState({
+      inputValue
+    })
+  }
+  handleSubmit(){
+    const {inputValue,list} = this.state
+    if(inputValue){
+      list.push(inputValue)
+      this.setState({
+        list,
+        inputValue:''
+      })
+    }else{
+      alert('please input some data')
+    }
+
+  }
+  handleliClick(index){
+    const {list} = this.state
+    list.splice(index,1)
+    this.setState({
+      list
+    })
+  }
 }
 
 export default App;
---
+++ src/SisComponet.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+
+class SisComponet extends Component{
+  render() {
+    return(
+      <div>
+      </div>
+    )
+  }
+}
+
+export default SisComponet
